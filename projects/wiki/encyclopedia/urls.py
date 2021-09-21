b74963a8d1843763c409fee272426f96d59b08c9
Sue <eungang3@naver.com>
2021-09-22 00:42:41 +0900

    finishe search

--- projects/wiki/encyclopedia/urls.py
+++ projects/wiki/encyclopedia/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
--- projects/wiki/encyclopedia/views.py
+++ projects/wiki/encyclopedia/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+import markdown2
 from . import util
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="title", widget=forms.TextInput(attrs={'placeholder': 'Enter title', 'style':'width:30%; margin-bottom: 1rem;'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Enter contents', 'style':'width:80%; height:500px; margin-bottom: 1rem;'}), label="content")
 
 
 def index(request):
@@ -9,4 +16,54 @@ def index(request):
     })
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            # check if the entry already exists
+            entries = util.list_entries()
+            for entry in entries:
+                if entry.lower() == title.lower():
+                    return render(request, "encyclopedia/error.html", {
+                        "message" : "Entry already Exists. Try editing the existing entry."
+                    })
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+    else:
+        return render(request, "encyclopedia/create.html", {
+            "form" : NewEntryForm()
+         })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html", {
+            "message" : "Entry does not exist."
+        })
+    html_entry = markdown2.markdown(entry)
+    return render(request, "encyclopedia/entry.html", {
+        "title" : title,
+        "entry" : html_entry
+    })
+
+def search(request):
+    query = request.POST["q"].lower()
+    entries = util.list_entries()
+    matches = []
+    for entry in entries:
+        # if there's a perfect match, redirect to that entry
+        if entry.lower() == query:
+            return render(request, "encyclopedia/entry.html", {
+                "title" : entry,
+                "entry" : markdown2.markdown(util.get_entry(entry))
+            })
+        # check if query is entry's substring
+        if query in entry.lower():
+            matches.append(entry)
+    # if there's no perfect match, show search result page        
+    return render(request, "encyclopedia/search.html", {
+        "query": query,
+        "matches":matches
+    })
