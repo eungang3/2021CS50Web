175328112326716f607a53f1a5e1a7aa3cd33c3f
Sue <eungang3@naver.com>
2021-10-03 18:51:54 +0900

    config admin

--- sandbox/lecture3/todo/todolist/models.py
+++ sandbox/lecture3/todo/todolist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=64)
+
+class Category(models.Model):
+    CATEGORY_LIST = (
+        ('[패키지]', '패키지'),
+        ('[마케팅]', '마케팅')
+    )
+    name = models.CharField(max_length=64, choices=CATEGORY_LIST)
