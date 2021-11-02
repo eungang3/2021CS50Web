f7238f5e85b506f1c8f727304f1ab6385a5cf9f8
Sue <eungang3@naver.com>
2021-11-03 00:20:08 +0900

    added

--- sandbox/lecture6/singlepage/post/views.py
+++ sandbox/lecture6/singlepage/post/views.py
@@ -1,5 +1,10 @@
+import time
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, "post/index.html")
+
+def posts(request):
+    
