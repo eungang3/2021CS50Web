7a31b5c4550f80a663de80423cf7f86315551971
Sue <eungang3@naver.com>
2021-11-06 02:00:18 +0900

    added view

--- sandbox/lecture6/singlepage/post/views.py
+++ sandbox/lecture6/singlepage/post/views.py
@@ -7,4 +7,12 @@ def index(request):
     return render(request, "post/index.html")
 
 def posts(request):
-    return render(request, )
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get("end") or start + 9)
+    data = []
+    for i in range(start, end + 1):
+        data.append(f"Post #{i}")
+    time.sleep(1)
+    return JsonResponse({
+        "posts" : data
+    })
